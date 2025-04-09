5154e98f9ee7b757c8071d8eaeea53f33b09dd54
Khyathi Maddala <maddalakhyathi@gmail.com>
2025-04-10 01:12:58 +0530

    Final working code: Updated main.tf

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -2,10 +2,30 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh_sg"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "web" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (update as needed)
-  instance_type = "t2.micro"
-  key_name      = "paper-social-key"      # Replace with your key pair name
+  ami                    = "ami-01f5a0b78d6089704"
+  instance_type          = "t2.micro"
+  key_name               = "paper-social-key"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   tags = {
     Name = "paper-social-web"
