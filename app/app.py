2b0368e548a684f8b8b733d0eb6ddb5f5f4dd389
Khyathi Maddala <maddalakhyathi@gmail.com>
2025-04-16 21:51:47 +0530

    Add CloudWatch Agent and config for monitoring

--- app/app.py
+++ app/app.py
@@ -1,9 +1,20 @@
 from flask import Flask
+import logging
+
 app = Flask(__name__)
+
+# Configure logging
+logging.basicConfig(
+    filename='/home/ec2-user/app/app.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
 @app.route('/')
 def home():
+    logging.info('Request received for homepage')
     return "Welcome to Paper.Social!"
 
 if __name__ == "__main__":
+    logging.info('Application started')
     app.run(host='0.0.0.0', port=5000)
